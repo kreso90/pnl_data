87be43ad9ddeaf155bc6007bb6c2475946cce3fc
kreso90 <kresimir.selendic@vivelacar.com>
2025-03-26 14:01:46 +0100

    get fix

--- src/app/api/pnl_single/[pnl_data_type]/route.ts
+++ src/app/api/pnl_single/[pnl_data_type]/route.ts
@@ -1,11 +1,17 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/db";
 
-export async function GET(req: Request, { params }: { params: { pnl_data_type: string } }) {
+interface MyResponseType extends NextResponse {
+    params: { pnl_data_type: string };
+}
+
+export async function GET (req: Request, res: MyResponseType) {
     try {
         const client = await clientPromise;
         const db = client.db("pnl_data");
 
+        const { params } = res;
+      
         const singleData = await db.collection("data_type").findOne({ pnl_data_type: params.pnl_data_type });
 
         return NextResponse.json(singleData);
