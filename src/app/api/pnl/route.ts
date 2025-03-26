c48d73aa591c29415ee6a7eee4f3e69dc7da7da7
kreso90 <kresimir.selendic@vivelacar.com>
2025-03-26 14:10:10 +0100

    get fix

--- src/app/api/pnl/route.ts
+++ src/app/api/pnl/route.ts
@@ -1,14 +1,13 @@
 import { NextResponse } from "next/server";
 import clientPromise from "@/lib/db";
 
-export async function GET({ params }: { params: { pnl_data_type: string } }) {
+export async function GET() {
     try {
-        
         const client = await clientPromise;
         const db = client.db("pnl_data");
 
-        console.log(params?.pnl_data_type)
         const data = await db.collection("data_type").find({}).toArray();
+        
         return NextResponse.json(data);
     } catch (error) {
         return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
--- src/app/api/pnl_single/[pnl_data_type]/route.ts
+++ src/app/api/pnl_single/[pnl_data_type]/route.ts
@@ -5,14 +5,15 @@ interface MyResponseType extends NextResponse {
     params: { pnl_data_type: string };
 }
 
-export async function GET (req: Request, res: MyResponseType) {
+export async function GET (req: Request) {
     try {
         const client = await clientPromise;
         const db = client.db("pnl_data");
 
-        const { params } = res;
+        const parts = req.url.split("/");
+        const pnl_data_type = parts[parts.length - 1];
       
-        const singleData = await db.collection("data_type").findOne({ pnl_data_type: params.pnl_data_type });
+        const singleData = await db.collection("data_type").findOne({ pnl_data_type: pnl_data_type });
 
         return NextResponse.json(singleData);
     } catch (error) {
